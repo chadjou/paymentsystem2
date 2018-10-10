603513967defb36ea8e82b5dae68633ca9e85676
Unknown <vladislav.kov@gmail.com>
2018-10-10 05:33:42 +0300

    firstCommit

---
+++ PaymentSystemServer/Models/PaymentSystemContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentSystemServer.Models
+{
+    public class PaymentSystemContext : DbContext
+    {
+
+        public PaymentSystemContext(DbContextOptions<PaymentSystemContext> options) : base(options)
+        { }
+
+        public DbSet<Seller> Sellers { get; set; }
+        public DbSet<Terminal> Terminals { get; set; }
+    }
+}
---
+++ PaymentSystemServer/Models/Terminal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentSystemServer.Models
+{
+    public class Terminal
+    {
+        public int TerminalId { get; set; }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public int SellerId { get; set; }
+        public virtual Seller Seller { get; set; }
+
+    }
+}
